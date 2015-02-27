1d4478f36850471f4016cc0f62adf5c32807d511
Yenny Cheung <yennycheung@hotmail.com>
2015-02-27 22:28:10 +0100

    finished frontpage

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ $(function() {
 	function resizingElements(){
 	    var height = $(window).innerHeight();
 	    $(".frontpage").css("height", height-50);
+	    var navHeight = $(".navigator").height();
+	    $(".navigator").css("top", (height-navHeight)/2);
 	}
 	resizingElements();
 	$(window).resize(function(event){
