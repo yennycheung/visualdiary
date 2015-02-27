d76e1814d192535a7a218a04901a38b289b772a2
Yenny Cheung <yennycheung@hotmail.com>
2015-02-27 23:30:57 +0100

    finished till 6

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ $(function() {
 	    $(".frontpage").css("height", height-50);
 	    var navHeight = $(".navigator").height();
 	    $(".navigator").css("top", (height-navHeight)/2);
+	    $(".story").css("height", height);
+	    var storyHeight = $(".story .container").height();
+	    $(".story .container").css("padding-top",(height-storyHeight)/3);
 	}
 	resizingElements();
 	$(window).resize(function(event){
